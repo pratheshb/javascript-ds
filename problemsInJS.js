9fd4c2f5b5ed455356fda56db15e115d72bf715d
Prathesh Baskaran <Prathesh.Baskaran@genesys.com>
2022-10-17 11:04:13 +0530

    reverseStr and BinarySearchToFindIndex

--- problemsInJS.js
+++ problemsInJS.js
@@ -1,45 +1,5 @@
-function binaryTree(value) {
-    const node = { value };
-    node[Symbol.iterator] = function* () {
-        yield this.value;
-        if(this.leftChild) yield* this.leftChild;
-        if(this.rightChild) yield* this.rightChild;
-    }
-    return node;
-}
-
-function makeTree() {
-    const tree = binaryTree('root');
-    tree.leftChild = binaryTree('left branch');
-    tree.rightChild = binaryTree('right branch');
-    tree.leftChild.leftChild = binaryTree('left branch left leaf');
-    tree.leftChild.rightChild = binaryTree('left branch right leaf');
-    tree.rightChild.leftChild = binaryTree('right branch left leaf');
-    return tree;
-}
-
-
-const tree = makeTree();
-
-const results = [];
-
-const generateStarWarsData = endPoint => async function* () {
-        let nextUrl = `https://swapi.dev/api/${endPoint}`;
-        while(nextUrl) {
-            const response = await fetch(nextUrl);
-            const data = await response.json();
-            nextUrl = data.next;
-            yield* data.results;
-        }
-        
-};
-
-const asyncGen = generateStarWarsData('people');
-
-(async () => { for await (let char of asyncGen()) { console.log(char.name)}})();
-
 let promise1 = new Promise(resolve => setTimeout(() => resolve('after 10 sec'), 10000));
-(async () => { let res = await promise1; console.log(res)})();
+(async () => { let res = await promise1; console.log(res) })();
 
 
 function cloneDeep(object) {
@@ -54,17 +14,17 @@ function isDeepEqual(obj1, obj2) {
     const obj1Type = typeof obj1;
     const obj2Type = typeof obj2;
 
-    if(obj1Type !== obj2Type) return false;
+    if (obj1Type !== obj2Type) return false;
 
-    if(obj1Type !== 'object' || obj1 === null || obj2 === null) return obj1 === obj2;
+    if (obj1Type !== 'object' || obj1 === null || obj2 === null) return obj1 === obj2;
 
-    if(Array.isArray(obj1) !== Array.isArray(obj2)) return false;
+    if (Array.isArray(obj1) !== Array.isArray(obj2)) return false;
 
-    if(Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
 
-    for(let key in obj1) {
-        if(!obj2.hasOwnProperty(key)) return false;
-        if(!isDeepEqual(obj1[key], obj2[key])) return false;
+    for (let key in obj1) {
+        if (!obj2.hasOwnProperty(key)) return false;
+        if (!isDeepEqual(obj1[key], obj2[key])) return false;
     }
 
     return true;
@@ -72,7 +32,7 @@ function isDeepEqual(obj1, obj2) {
 
 
 function getDiff(obj1, obj2) {
-    const res = {created: {}, updated: {}};
+    const res = { created: {}, updated: {} };
     for (let key in obj2) {
         const value1 = obj1[key];
         const value2 = obj2[key];
@@ -100,7 +60,7 @@ function promiseAll(promises) {
             promise.then(res => {
                 result[i] = res;
                 resolvedPromiseCount++;
-                if(resolvedPromiseCount === promises.length) {
+                if (resolvedPromiseCount === promises.length) {
                     resolve(result);
                 }
             }).catch(reject);
@@ -117,3 +77,55 @@ function promiseRace(promises) {
         });
     });
 }
+
+
+function reverseStr(str) {
+    if (str.length === 1) {
+        return str;
+    }
+    let mid = Math.floor(str.length / 2);
+    let firstHalf = str.slice(0, mid);
+    let secondHalf = str.slice(mid, str.length);
+    return reverseStr(secondHalf) + reverseStr(firstHalf);
+}
+
+
+function promiseAllSetteled(promises) {
+    const output = [];
+    let count = 0;
+    const resolveFn = (i, resolve, value, isResolved) => {
+        output[i] = isResolved ? { status: 'fulfilled', value } : { status: 'rejected', reason: value };
+        count++;
+        if (count === promises.length) {
+            resolve(output);
+        }
+    };
+    return new Promise(resolve => {
+        promises.forEach((promise, i) => {
+            promise.then(value => {
+                resolveFn(i, resolve, value, true);
+            }).catch(reason => {
+                resolveFn(i, resolve, reason, false);
+            });
+        });
+    });
+}
+
+function findIndexByBinarySearch(arr, elm, i) {
+    if (!arr.length) {
+        return -1;
+    }
+    if (typeof i !== 'number') {
+        i = 0;
+    }
+    let mid = Math.floor(arr.length / 2);
+    if (arr[mid] === elm) {
+        return i + mid;
+    } else if (elm < arr[mid]) {
+        return findIndexByBinarySearch(arr.slice(0, mid), elm, i);
+    } else {
+        return findIndexByBinarySearch(arr.slice(mid + 1), elm, i + mid + 1);
+    }
+}
+
+findIndexByBinarySearch([0, 1, 2, 3, 4, 5], 5) // 5
